eed291a0983965c25f28f68fec4afb5c58def87c
MarinaOrzechowski <m.skachko@hotmail.com>
2020-08-01 17:31:05 -0400

    adds input fields: age, weight, planet
    - adds input fields to the 'data' page for user to input their age,
    weight, planet
    - calculates the weight of the user on the selected planet

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+gravity_dict = {'Mercury': 0.38, 'Venus': 0.91, 'Mars': 0.38,
+                "Jupiter": 2.34, 'Saturn': 0.93, 'Uranus': 0.92, 'Neptune': 1.12}
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -15,6 +18,15 @@ def data():
 
 @app.route("/result", methods=['POST', 'GET'])
 def result():
+    if request.method == 'POST':
+        # extract user input
+        image = request.files.get('img_name')
+        age = request.form.get('age')
+        weight = request.form.get('weight')
+        planet = request.form.get('planet_dropdown')
+        print(age, weight, planet)
+        new_weight = weight*gravity_dict[planet]
+
     return render_template("result.html")
 
 
