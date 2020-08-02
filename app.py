d552454c098bcd3c063936e741760d0416048271
MarinaOrzechowski <m.skachko@hotmail.com>
2020-08-01 23:03:40 -0400

    fixing security issue (github raw data link)

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, request
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
 gravity_dict = {'Mercury': 0.38, 'Venus': 0.91, 'Mars': 0.38,
                 "Jupiter": 2.34, 'Saturn': 0.93, 'Uranus': 0.92, 'Neptune': 1.12}
+time_to_orbit_Sun1 = {'Mercury': 88, 'Venus': 225, 'Mars': 687}
+time_to_orbit_Sun2 = {"Jupiter": 11.8,
+                      'Saturn': 29.4, 'Uranus': 84, 'Neptune': 164}
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # read astronaut data into pandas dataframe
-df = pd.read_csv(
-    'https://raw.githubusercontent.com/MarinaOrzechowski/AstroNOT/master/static/US_astronauts.csv')
+df = pd.read_csv(dir_path +
+                 '\\static\\US_astronauts.csv')
+print(dir_path)
 df = df[pd.notnull(df['Year'])]
 df['birth_year'] = df['Birth Date'].astype('str').str[-4:].astype(int)
 df['age'] = df['Year'] - df['birth_year']
@@ -33,15 +40,22 @@ def data():
 def result():
     if request.method == 'POST':
         # extract user input
-        image = request.files.get('img_name')
         age = request.form.get('age')
         weight = request.form.get('weight')
         planet = request.form.get('planet_dropdown')
+
+        # calculate user's weight on the chosen planet
         if '.' in weight:
             weight = float(weight)
         else:
             weight = int(weight)
-        new_weight = weight*gravity_dict[planet]
+        new_weight = round(weight*gravity_dict[planet], 2)
+
+        # calculate user's age on the chosen planet
+        if planet in time_to_orbit_Sun1:
+            new_age = round(int(age)*365/time_to_orbit_Sun1[planet], 2)
+        else:
+            new_age = round(int(age)/time_to_orbit_Sun2[planet], 2)
 
         # find an astronaut of similar age
         astronauts = df[df['age'].astype(int) == int(age)]
@@ -66,7 +80,7 @@ def result():
         print(a_name, a_year_went_to_space, a_birth_date, a_birth_place, a_college,
               a_major_b, a_major_g, a_flights, a_flights_hr, a_walks, a_walks_hr, a_alive)
 
-    return render_template("result.html", planet_result=planet, weight_result=new_weight, a_name=a_name, a_year_went_to_space=a_year_went_to_space, a_birth_date=a_birth_date, a_birth_place=a_birth_place, a_college=a_college, a_major_b=a_major_b, a_major_g=a_major_g, a_flights=a_flights, a_flights_hr=a_flights_hr, a_walks=a_walks, a_walks_hr=a_walks_hr, a_alive=a_alive, message_astronaut=message_astronaut)
+    return render_template("result.html", planet_result=planet, weight_result=new_weight, a_name=a_name, a_year_went_to_space=a_year_went_to_space, a_birth_date=a_birth_date, a_birth_place=a_birth_place, a_college=a_college, a_major_b=a_major_b, a_major_g=a_major_g, a_flights=a_flights, a_flights_hr=a_flights_hr, a_walks=a_walks, a_walks_hr=a_walks_hr, a_alive=a_alive, message_astronaut=message_astronaut, new_age=new_age)
 
 
 if __name__ == '__main__':
